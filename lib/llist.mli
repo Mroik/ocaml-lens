256661978a09251ace2e64b7d5455c20a881c4a5
Mroik <mroik@delayed.space>
2025-02-08 01:00:01 +0100

    Add raised exception to under operator docs

--- lib/llist.mli
+++ lib/llist.mli
@@ -17,4 +17,5 @@ val ( >>| ) :
   ('a list -> 'a list) -> ('b -> 'a list_lens_internals) -> 'b -> 'a list
 (** Returns a new function that uses the one on the right to pre-process the
     list, applies the one on the left on the result and then undoes the
-    pre-processing. *)
+    pre-processing. The function doing the modifying has to keep the same
+    shape of the pre-processed list, raises NotSameShapeException if otherwise.*)
